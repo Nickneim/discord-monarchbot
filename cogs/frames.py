9990f4ac8dad7e899ed199b615f9b53211552bff
nickneim <nicolasmontes@gmail.com>
2019-01-14 14:05:32 -0300

    added addframe and addtemplate commands, changed how frames and templates are listed, added check_template command

--- cogs/frames.py
+++ cogs/frames.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from PIL import Image
+from PIL import Image, ImageDraw
 from .image_handling import get_image, send_image, get_frame
 
 
@@ -18,48 +18,61 @@ def image_inside_frame(image, frame, box):
     frame.paste(frame_crop, box, frame_crop)
 
 
+def detect_template(image):
+    """
+    Detect an area of transparency and return the coordinates of the rectangle.
+    If the image has no alpha channel returns None.
+    :param image: Image
+    :return: An (int, int, int, int) tuple or None.
+    """
+    try:
+        image = image.getchannel('A')
+    except ValueError:
+        return None
+    x1 = image.width
+    y1 = image.height
+    x2 = 0
+    y2 = 0
+    i = 0
+    for pixel in image.getdata():
+        if pixel != 255:
+            x = i % image.width
+            y = i // image.width
+            if x < x1:
+                x1 = x
+            if y < y1:
+                y1 = y
+            if x > x2:
+                x2 = x
+            if y > y2:
+                y2 = y
+        i += 1
+    return x1, y1, x2, y2
+
+
 class Frames:
 
-    frames = ['ac', 'acab', 'al', 'amouranth', 'bann', 'beatles', 'bjork', 'blond',
-              'blood', 'bop', 'brazzers', 'bulge', 'can', 'cat', 'cj1', 'cnid',
-              'coil1', 'coil2', 'comic1', 'comic2', 'commie', 'conway', 'cooper1', 'cooper2', 'cowboy',
-              'crosshair', 'cry', 'dab1', 'damn', 'dg', 'dick', 'die', 'dole', 'elon', 'forklift', 'fuckyou',
-              'funwaa', 'gag', 'garf2', 'garf3', 'garf4', 'garf5', 'gay',
-              'general', 'grinch', 'hhill', 'hillary', 'idub1', 'idub2', 'idub3', 'idub4', 'idub5', 'idub6',
-              'ifunny', 'jempy', 'jf1', 'jf2', 'kim', 'knife', 'knuckle', 'liberal', 'lilpeep', 'lilpump',
-              'lrd', 'lynch', 'madvillainy', 'mark', 'meme1', 'nmh',
-              'obama2', 'parental', 'pickle', 'piss', 'preg', 'qotsa', 'rh', 'sam',
-              'sb', 'shapiro', 'shrek', 'shutterstock', 'sketch', 'socks', 'sonic', 'spaghett',
-              'spongebob', 'ss', 'swans', 'swear1', 'swear2', 'sy', 'television', 'testframe', 'tmay',
-              'tp', 'trigger', 'trump', 'unlock', 'vine1', 'vu', 'warcrime', 'wasted', 'wish',
-              'wtc2', 'zucc', 'zucc1', 'zucc2', 'zucc3', 'zucc4']
+    try:
+        with open("frames/frames.txt") as f:
+            frames = f.read().split()
+    except FileNotFoundError:
+        print("Couldn't find frames/frames.txt.")
+        frames = []
 
-    templates = {
-        # name: (left, top, right, bottom),
-        'altright': (9, 195, 897, 687),
-        'buzzfeed': (16, 86, 587, 416),
-        'calvin': (0, 0, 300, 28),
-        'clash': (59, 0, 342, 225),
-        'cruz': (75, 26, 459, 344),
-        'funeral': (46, 55, 356, 183),
-        'garf1': (35, 48, 264, 238),
-        'gccx1': (0, 49, 640, 431),
-        'gccx2': (0, 19, 259, 173),
-        'jones': (503, 0, 1187, 717),
-        'mars': (204, 0, 534, 246),
-        'memri': (28, 9, 661, 297),
-        'n64': (119, 67, 377, 325),
-        'nut': (0, 87, 714, 633),
-        'obama1': (7, 90, 448, 378),
-        'picard': (0, 0, 600, 307),
-        'porn': (15, 82, 776, 502),
-        'rapper': (0, 66, 522, 510),
-        'science': (0, 147, 1000, 833),
-        'son': (0, 86, 569, 613),
-        'testtemplate': (50, 90, 537, 285),
-        'wedding': (158, 12, 394, 257),
-        'wtc1': (0, 0, 300, 238),
-    }
+    try:
+        templates = {}
+        with open("frames/templates.txt") as f:
+            for line in f:
+                template, *coords = line.split()
+                try:
+                    x1, y1, x2, y2 = [int(x) for x in coords]
+                except ValueError as e:
+                    print(e)
+                else:
+                    templates[template] = (x1, y1, x2, y2)
+    except FileNotFoundError:
+        print("Couldn't find frames/templates.txt")
+        templates = {}
 
     def __init__(self, bot):
         self.bot = bot
@@ -67,6 +80,7 @@ class Frames:
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command(aliases=frames)
     async def frame(self, ctx):
+        """Paste the frame on the last image."""
         frame_name = ctx.invoked_with
         if frame_name == 'frame':
             return
@@ -84,6 +98,7 @@ class Frames:
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command(aliases=list(templates))
     async def template(self, ctx):
+        """Insert the last image in the template."""
         frame_name = ctx.invoked_with
         if frame_name == 'template':
             return
@@ -101,6 +116,7 @@ class Frames:
     @commands.cooldown(1, 5, commands.BucketType.default)
     @commands.command()
     async def addtestframe(self, ctx):
+        """Download the last image and add it as a test frame."""
         test_image, image_message = await get_image(ctx)
         if not test_image:
             return
@@ -114,6 +130,7 @@ class Frames:
     @commands.cooldown(1, 5, commands.BucketType.default)
     @commands.command()
     async def addtesttemplate(self, ctx, x1: int, y1: int, x2: int, y2: int):
+        """Download the last image and add it as a test template."""
         if x1 >= x2 or y1 >= y2 or x1 < 0 or y1 < 0:
             return await ctx.send("Those coordinates don't seem valid.")
 
@@ -128,38 +145,103 @@ class Frames:
             Frames.templates['testtemplate'] = (x1, y1, x2, y2)
         except IOError:
             return await ctx.send("Image couldn't be saved.")
-        return await ctx.send("Changed test frame")
+        return await ctx.send("Changed test template.")
 
     @commands.cooldown(1, 5, commands.BucketType.default)
+    @commands.command(name="addframe")
+    async def _add_frame(self, ctx, frame_name: str):
+        """Download the last image and add it as a frame."""
+        if self.bot.get_command(frame_name):
+            return await ctx.send("There's already a command with that name.")
+        frame, frame_message = await get_image(ctx)
+        if not frame:
+            return
+        try:
+            frame.convert('RGBA').save(f'frames/{frame_name}.png')
+        except IOError:
+            return await ctx.send("Couldn't save the image.")
+
+        with open('frames/frames.txt', 'a') as f:
+            f.write(f'\n{frame_name}')
+        command = self.bot.get_command('frame')
+        # just to be pretty, not actually important
+        command.aliases.append(frame_name)
+        # this is what actually does the work
+        self.bot.all_commands[frame_name] = command
+        await ctx.send(f"Added frame {frame_name}.")
+
+    @commands.cooldown(1, 5, commands.BucketType.default)
+    @commands.command(name="addtemplate")
+    async def _add_template(self, ctx, template_name: str, x1: int, y1: int, x2: int, y2: int):
+        """Download the last image and add it as a template with rectangle (x1, y1, x2, y2)."""
+        if self.bot.get_command(template_name):
+            return await ctx.send("There's already a command with that name.")
+        if x1 >= x2 or y1 >= y2 or x1 < 0 or y1 < 0:
+            return await ctx.send("Those coordinates don't seem valid.")
+
+        template, template_message = await get_image(ctx)
+        if not template:
+            return
+        if template.width < x2 or template.height < y2:
+            return await ctx.send("Box goes outside of template.")
+
+        try:
+            template.convert('RGBA').save(f'frames/{template_name}.png')
+        except IOError:
+            return await ctx.send("Couldn't save the image.")
+
+        with open('frames/templates.txt', 'a') as f:
+            f.write(f'\n{template_name} {x1} {y1} {x2} {y2}')
+        command = self.bot.get_command('template')
+        # just to be pretty, not actually important
+        command.aliases.append(template_name)
+        # this is what actually does the work
+        Frames.templates[template_name] = (x1, y1, x2, y2)
+        self.bot.all_commands[template_name] = command
+        await ctx.send(f"Added template {template_name} with box ({x1}, {y1}, {x2}, {y2}).")
+
+    @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command()
-    async def detecttemplate(self, ctx):
+    async def check_template(self, ctx, template_name, x1: int = None, y1: int = None, x2: int = None, y2: int = None):
+        """Draw a rectangle in a template. Rectangle defaults to the template's rectangle."""
+        if template_name not in Frames.templates or template_name == 'template':
+            return await ctx.send("There's no template with that name.")
+        if x1 is not None:
+            if y2 is None:
+                return await ctx.send("Coordinates are partial and not valid.")
+            elif x1 >= x2 or y1 >= y2 or x1 < 0 or y1 < 0:
+                return await ctx.send("Those coordinates don't seem valid.")
+        else:
+            x1, y1, x2, y2 = Frames.templates[template_name]
+
+        frame = await get_frame(ctx, template_name)
+        if not frame:
+            return
+
+        draw = ImageDraw.Draw(frame)
+        draw.rectangle((x1, y1, x2, y2), outline=(255, 0, 0, 255), width=2)
+
+        await send_image(ctx, frame, ctx.message)
+
+    @commands.is_owner()
+    @commands.command(name='eval', hidden=True)
+    async def _eval(self, ctx, *, to_eval: str):
+        print(eval(to_eval))
+
+    @commands.cooldown(1, 5, commands.BucketType.default)
+    @commands.command(name="detecttemplate")
+    async def _detect_template(self, ctx):
+        """Detects a transparent rectangle for the last image as a template and draws it."""
         image, image_message = await get_image(ctx)
         if not image:
             return
-        try:
-            image = image.getchannel('A')
-        except ValueError:
+        coords = detect_template(image)
+        if coords is None:
             return await ctx.send("That image doesn't have an alpha channel.")
-        x1 = image.width
-        y1 = image.height
-        x2 = 0
-        y2 = 0
-        i = 0
-        for pixel in image.getdata():
-            if pixel != 255:
-                x = i % image.width
-                y = i // image.width
-                if x < x1:
-                    x1 = x
-                if y < y1:
-                    y1 = y
-                if x > x2:
-                    x2 = x
-                if y > y2:
-                    y2 = y
-            i += 1
-
-        await ctx.send(f"({x1}, {y1}, {x2}, {y2})")
+        draw = ImageDraw.Draw(image)
+        draw.rectangle(coords, outline=(255, 0, 0, 255), width=2)
+        await send_image(ctx, image, ctx.message)
+        await ctx.send(str(coords))
 
 
 def setup(client):
