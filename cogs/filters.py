555a9ecb3c42f799f9380696812b36dd07f41f11
nickneim <nicolasmontes@gmail.com>
2019-03-13 14:36:39 -0300

    made filters into their own functions

--- cogs/filters.py
+++ cogs/filters.py
@@ -34,67 +34,154 @@ def wrap_text(draw, font, text, width):
     return result.rstrip()
 
 
+def blur(image):
+    return image.filter(ImageFilter.BLUR)
+
+
+def invert(image):
+    try:
+        alpha = image.getchannel('A')
+    except ValueError:
+        alpha = None
+    image = image.convert("RGB")
+    image = ImageOps.invert(image)
+    if alpha:
+        image.putalpha(alpha)
+    return image
+
+
+def flipv(image):
+    return ImageOps.flip(image)
+
+
+def fliph(image):
+    return ImageOps.mirror(image)
+
+
+def symm(image, side='left'):
+    w, h = image.size
+    if side == 'left':
+        crop = image.crop((0, 0, w // 2, h))
+        crop = ImageOps.mirror(crop)
+        image.paste(crop, (round(w / 2 + 0.1), 0))
+    elif side == 'right':
+        crop = image.crop((round(w / 2 + 0.1), 0, w, h))
+        crop = ImageOps.mirror(crop)
+        image.paste(crop, (0, 0))
+    elif side == 'up':
+        crop = image.crop((0, 0, w, h // 2))
+        crop = ImageOps.flip(crop)
+        image.paste(crop, (0, round(h / 2 + 0.1)))
+    elif side == 'down':
+        crop = image.crop((0, round(h / 2 + 0.1), w, h))
+        crop = ImageOps.flip(crop)
+        image.paste(crop, (0, 0))
+    return image
+
+
+def posterize(image):
+    try:
+        alpha = image.getchannel('A')
+    except ValueError:
+        alpha = None
+    image = image.convert("RGB")
+    image = ImageOps.posterize(image, 2)
+    if alpha:
+        image.putalpha(alpha)
+    return image
+
+
+def shrink(image, per=5):
+    image = image.convert("RGBA")
+    w = image.width * per // 100
+    h = image.height * per // 100
+    image = image.resize((w, h), resample=Image.LANCZOS)
+    return image
+
+
+def glitch(image):
+    w, h = image.width, image.height
+    image = image.resize((int(w ** .75), int(h ** .75)), resample=Image.LANCZOS)
+    image = image.resize((int(w ** .88), int(h ** .88)), resample=Image.BILINEAR)
+    image = image.resize((int(w ** .9), int(h ** .9)), resample=Image.BICUBIC)
+    image = image.resize((w, h), resample=Image.BICUBIC)
+    try:
+        alpha = image.getchannel('A')
+    except ValueError:
+        alpha = None
+    image = image.convert("RGB")
+    image = ImageOps.posterize(image, 4)
+    if alpha:
+        image.putalpha(alpha)
+    image = ImageEnhance.Sharpness(image).enhance(100.0)
+    return image
+
+
+def edges(image):
+    try:
+        alpha = image.getchannel('A')
+    except ValueError:
+        alpha = None
+    image = image.filter(ImageFilter.SMOOTH)
+    image = image.filter(ImageFilter.CONTOUR)
+    image = image.convert("RGB")
+    image = ImageOps.invert(image)
+    image = ImageOps.grayscale(image)
+    image = image.convert("RGBA")
+    if alpha:
+        image.putalpha(alpha)
+    return image
+
+
+def rotate(image, rotation='right'):
+    if rotation == 'left':
+        return image.transpose(Image.ROTATE_90)
+    else:
+        return image.transpose(Image.ROTATE_270)
+
+
+filters = {
+    'blur': blur,
+    'invert': invert,
+    'flipv': flipv,
+    'fliph': fliph,
+    'posterize': posterize,
+    'glitch': glitch,
+    'edges': edges,
+}
+
+advanced_filters = {
+    'symm': symm,
+    'shrink': shrink,
+    'rotate': rotate,
+}
+
+
 class Filters:
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.cooldown(1, 5, commands.BucketType.user)
-    @commands.command()
-    async def blur(self, ctx):
+    @commands.command(aliases=list(filters))
+    async def filter(self, ctx):
+        filter_name = ctx.invoked_with
+        if filter_name == 'filter':
+            return
         image, image_message = await get_image(ctx)
         if not image:
             return
 
-        image = image.filter(ImageFilter.BLUR)
+        image = filters[filter_name](image)
 
-        await send_image(ctx, image, image_message)
-
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    @commands.command()
-    async def invert(self, ctx):
-        image, image_message = await get_image(ctx)
-        if not image:
-            return
-
-        try:
-            alpha = image.getchannel('A')
-        except ValueError:
-            alpha = None
-        image = image.convert("RGB")
-        image = ImageOps.invert(image)
-        if alpha:
-            image.putalpha(alpha)
-
-        await send_image(ctx, image, image_message)
-
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    @commands.command()
-    async def flipv(self, ctx):
-        image, image_message = await get_image(ctx)
-        if not image:
-            return
-
-        image = ImageOps.flip(image)
-
-        await send_image(ctx, image, image_message)
-
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    @commands.command()
-    async def fliph(self, ctx):
-        image, image_message = await get_image(ctx)
-        if not image:
-            return
-
-        image = ImageOps.mirror(image)
         await send_image(ctx, image, image_message)
 
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command()
     async def symm(self, ctx, side: str = "left"):
         side = side.lower()
-        if side not in ('left', 'right', 'up', 'down'):
-            return await ctx.send("Valid sides are 'left', 'right', 'up', and 'down'.")
+        if side not in ('left', 'right', 'top', 'bottom'):
+            return await ctx.send("Valid sides are 'left', 'right', 'top', and 'bottom'.")
         image, image_message = await get_image(ctx)
         if not image:
             return
@@ -108,32 +195,14 @@ class Filters:
             crop = image.crop((round(w / 2 + 0.1), 0, w, h))
             crop = ImageOps.mirror(crop)
             image.paste(crop, (0, 0))
-        elif side == 'up':
+        elif side == 'top':
             crop = image.crop((0, 0, w, h//2))
             crop = ImageOps.flip(crop)
             image.paste(crop, (0, round(h / 2 + 0.1)))
-        elif side == 'down':
+        elif side == 'bottom':
             crop = image.crop((0, round(h / 2 + 0.1), w, h))
             crop = ImageOps.flip(crop)
             image.paste(crop, (0, 0))
-
-        await send_image(ctx, image, image_message)
-
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    @commands.command()
-    async def posterize(self, ctx):
-        image, image_message = await get_image(ctx)
-        if not image:
-            return
-
-        try:
-            alpha = image.getchannel('A')
-        except ValueError:
-            alpha = None
-        image = image.convert("RGB")
-        image = ImageOps.posterize(image, 2)
-        if alpha:
-            image.putalpha(alpha)
 
         await send_image(ctx, image, image_message)
 
@@ -149,52 +218,6 @@ class Filters:
         w = image.width * per // 100
         h = image.height * per // 100
         image = image.resize((w, h), resample=Image.LANCZOS)
-
-        await send_image(ctx, image, image_message)
-
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    @commands.command()
-    async def glitch(self, ctx):
-        image, image_message = await get_image(ctx)
-        if not image:
-            return
-
-        w, h = image.width, image.height
-        image = image.resize((int(w ** .75), int(h ** .75)), resample=Image.LANCZOS)
-        image = image.resize((int(w ** .88), int(h ** .88)), resample=Image.BILINEAR)
-        image = image.resize((int(w ** .9), int(h ** .9)), resample=Image.BICUBIC)
-        image = image.resize((w, h), resample=Image.BICUBIC)
-        try:
-            alpha = image.getchannel('A')
-        except ValueError:
-            alpha = None
-        image = image.convert("RGB")
-        image = ImageOps.posterize(image, 4)
-        if alpha:
-            image.putalpha(alpha)
-        image = ImageEnhance.Sharpness(image).enhance(100.0)
-
-        await send_image(ctx, image, image_message)
-
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    @commands.command()
-    async def edges(self, ctx):
-        image, image_message = await get_image(ctx)
-        if not image:
-            return
-
-        try:
-            alpha = image.getchannel('A')
-        except ValueError:
-            alpha = None
-        image = image.filter(ImageFilter.SMOOTH)
-        image = image.filter(ImageFilter.CONTOUR)
-        image = image.convert("RGB")
-        image = ImageOps.invert(image)
-        image = ImageOps.grayscale(image)
-        image = image.convert("RGBA")
-        if alpha:
-            image.putalpha(alpha)
 
         await send_image(ctx, image, image_message)
 
